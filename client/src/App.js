db5c964b979623ddc1ebc1f0b64d1c25250324f5
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2021-07-17 16:16:23 +0900

    Use react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import Home from "./Home";
 import SimpleAuctionApp from "./simple_auction/SimpleAuctionApp";
 import PaymentChannelApp from "./simple_payment_channel/PaymentChannelApp";
 
@@ -9,10 +11,35 @@ import getWeb3 from "./utils/getWeb3";
 class App extends Component {
   render() {
     return(
-      <div>
-        <SimpleAuctionApp />
-        <PaymentChannelApp />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/simple_auction">Simple Auction</Link>
+              </li>
+              <li>
+                <Link to ="/simple_payment_channel">Simple Payment Channel</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/simple_auction">
+              <SimpleAuctionApp />
+            </Route>
+            <Route path="/simple_payment_channel">
+              <PaymentChannelApp />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       );
   }
 }
